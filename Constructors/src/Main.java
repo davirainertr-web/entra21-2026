a6cc2b39b7755bb8703bc3230cd6570367f682c2
Davi Rainert Rosa <davirainert.r@gmail.com>
2026-07-02 16:41:20 -0300

    constructors

---
+++ Constructors/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+	  // Produto p = new Produto();
+	   //Produto p = new Produto(1,"Milho");
+		Produto p = new Produto(1,"Milho",0);
+	   System.out.println(p);
+	   
+	  // p.setId(1);
+	  // p.setDescricao("milho");
+	  // p.setPreco(1.75);
+	  // p.setEstoque(10);
+	   
+	   System.out.println(p.getId());
+	   System.out.println(p.getDescricao());
+	   System.out.println(p.getPreco());
+	   System.out.println(p.getEstoque());   
+
+	}
+
+}
