6c33efb7eb4d59ca9d41e894436be0d3b95b536a
Davi Rosa <davi63466176@alunos.sc.senac.br>
2026-09-03 13:52:20 -0300

    Adicionando projeto de Autenticação - Spring Security

---
+++ Autenticacao/src/test/java/com/security/Autenticacao/AutenticacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.Autenticacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutenticacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
